0e0ae497c9b4e0e25c71606a919d162a2cbf93da
Jacob Purcell <jacobdpurcell@outlook.com>
2019-04-21 20:51:53 -0700

    started playing with vectors

--- Test Area/Test Area/Source.cpp
+++ Test Area/Test Area/Source.cpp
@@ -7,27 +7,14 @@
 
 using namespace std;
 
-int counter = 0;
+
+string name;
+
+vector <string> NewHS (string inname, string inscore) ;
+
 
 int main()
 {
-	system("cls");
-	int x;
-	int b;
-	int a;
-	int s;
-	int d;
-	int f;
-
 	srand((unsigned)time(0));
-	x = rand() % 10;
-	b = rand() % 10;
-	a = rand() % 10;
-	s = rand() % 10;
-	d = rand() % 10;
-	f = rand() % 10;
-	counter++;
-	cout << x << endl << b << endl << a << endl << s << endl << d << endl << f << endl << endl << counter << endl << endl;
-	main();
-	
+	int score = rand() % 10;
 }
